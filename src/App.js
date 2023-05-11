e9751496978f7bca71654469f93c8c6ce592eb03
PrangonGhose <prangonghose@gmail.com>
2023-05-11 22:30:34 +0600

    Implement redux store to show random message on the view

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getGreetings } from "./redux/greetings";
 
 function App() {
+  const newVar = useSelector((state) => state.greetings)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getGreetings());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Random Greeting: {newVar} </h1>
+    </>
+  )
 }
 
 export default App;
